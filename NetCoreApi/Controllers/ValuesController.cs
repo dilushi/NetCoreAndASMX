ee537171220bad637ecc1a0d28a5d3260d5423c4
dilushi <niluka28@yahoo.com>
2018-10-03 06:35:08 +0100

    Added initial project.

---
+++ NetCoreApi/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestASMXServiceReference;
+
+namespace NetCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            EndpointAddress endPointAddress = new EndpointAddress("http://localhost:57429/WebService1.asmx");
+
+            //Send to LanDesk SOAP service.
+            WebService1SoapClient soapClient = new WebService1SoapClient(WebService1SoapClient.EndpointConfiguration.WebService1Soap, 
+                endPointAddress);
+            var response = soapClient.HelloWorldAsync().Result;            
+
+            return response.Body.HelloWorldResult;
+        }        
+    }
+}
